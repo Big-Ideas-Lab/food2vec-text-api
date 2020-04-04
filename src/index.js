48e9af88e0aca18e2e4f2eb817c7276127b7ccd0
Aman Ibrahim <amanmibra@gmail.com>
2020-04-04 17:10:13 -0400

    Added test spoon api call

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+
+require('dotenv').config()
 
 // start app
 const app = express();
 app.use(cors());
 
 // routes
-app.get('/', (_, res) => {
-  res.send('hello world!')
+app.get('/', async (_, res) => {
+  const key = process.env.API_KEY || false
+  if (!key) {
+    throw new Error('API Key Not Found.')
+  }
+
+  const url = `https://api.spoonacular.com/recipes/search?query=milkshake&number=2&apiKey=${key}`
+
+  axios.get(url)
+  .then(function (r) {
+    res.json(r.data)
+  })
+  .catch(function (err) {
+    console.log(err);
+  })
+
+  res.send('hello worlds!')
+
 });
 
 
 // listen
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`COVIDentify API listening on port ${port}!`);
+  console.log(`Food2Vec Text API listening on port ${port}!`);
 })
